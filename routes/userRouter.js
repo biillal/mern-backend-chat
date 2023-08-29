506194388fd518ff68f6b42283d8bdf810dc4d70
billal <boudjellbillal@gmail.com>
2023-08-29 14:12:05 +0200

    updimg

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -14,6 +14,6 @@ router.route('/:id')
     .get(getSingleUser)
     .delete(verifyTokenAdminAndUser, deleteUser)
     .put(verifyTokenAndOnlyUser, updateUser)
-router.route('/profile/profile-photo-upload')
+router.route('/profile-photo-upload')
     .post(verifyToken, upload.single("image"), profilePhotoUploadCntr)
 module.exports = router
